0b3e71af0c73fbff831c18443795c24695c70c60
SuperCoolNinja <sbpro570@gmail.com>
2024-04-08 12:01:43 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Jagged
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tableauUniforme = new int[3, 3] {
+                {1, 2, 3},
+                {2, 3, 4},
+                {3, 4, 5}
+            };
+
+            int[][] tableauIrregulier = new int[3][];
+
+            // En Remplie le tableau irregulier avec les valeurs du tableau uniforme : 
+            for (int r = 0; r < 3; r++)
+            {
+                int[] ligne = new int[tableauUniforme.GetLength(1) - r];
+                for (int c = 0; c < ligne.Length; c++)
+                    ligne[c] = tableauUniforme[r, c];
+
+                tableauIrregulier[r] = ligne;
+            }
+
+
+            // Affichage du tableau irregulier : 
+            for (int r = 0; r < tableauIrregulier.Length; r++)
+            {
+                Console.Write($"[{r} => [");
+                for (int c = 0; c < tableauIrregulier[r].Length; c++)
+                {
+                    Console.Write($"{tableauIrregulier[r][c]}");
+                    if (c < tableauIrregulier[r].Length - 1)
+                        Console.Write(", ");
+                }
+                Console.WriteLine("]]");
+            }
+
+
+        }
+    }
+}
